32d023ec995ff1fc600063e0b9f883f0a50339b9
Shannon Nolan <shannonnolan17@gmail.com>
2017-10-28 17:07:30 -0700

    Add css styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,10 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    @comment = @article.comments.new(comment_params)
+    if @comment.save
+      redirect_to article_path(@article)
+    end
   end
 
   def destroy
